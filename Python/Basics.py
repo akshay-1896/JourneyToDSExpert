cfa8f5c26463fe26cceca2f3482799006594df02
Akshay Sharma <officialakshay1896@gmail.com>
2024-09-10 19:33:16 +0530

    add new file

---
+++ Python/Basics.py
@@ -0,0 +1,30 @@
+# college = "   manipal  "
+
+# print(college.strip())
+# print(college.rstrip())
+# print(college.lstrip())
+
+# escape characters
+
+# text = "my name is "akshay"" --> throws errorf
+# text = "my name is \"akshay\""
+# print(text)
+
+# fruits = ["apple", "papaya", "orange", "mango"]
+# fruits[1:2] = ["blackcurrent", "cherry"]
+# fruits[1:3] = ["watermelon"]
+# print(fruits)
+
+# print(fruits[::-1]) # to reverse the list without any loop or function
+
+# lst = [[1,2,3],[4,5],[6,7]]
+# lst[1][1] = 10 # accessing items in embedded list
+# print(lst)
+
+# tuple constructor
+
+# tpl = tuple(("q","1","akshay"))
+tpl = ("blue",)
+print(tpl)
+print(type(tpl))
+
