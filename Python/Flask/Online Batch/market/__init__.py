42abd50aa68856c5b04cc71def5f7b86ee0ac6a4
Akshay Sharma <officialakshay1896@gmail.com>
2024-12-24 22:11:57 +0530

    new files added

--- Python/Flask/Online Batch/market/__init__.py
+++ Python/Flask/Online Batch/market/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' # URI--> uniform resource identifier to identify the file as a database
+app.config['SECRET_KEY'] = '89625f950c39e473f878556c'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager= LoginManager(app)
 from market import routes
